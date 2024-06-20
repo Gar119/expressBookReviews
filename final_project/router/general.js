9606913dba1d7d529c3bc1f9d6d03b3567591385
Gareth O'Leary <G00375321@gmit.ie>
2024-06-20 13:36:00 -0500

    Completed task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,18 +33,37 @@ function getBooks() {
   });
 }
 
-// Get the book list available in the shop
 public_users.get('/', async function (req, res) {
   const book = await getBooks();
   res.send(JSON.stringify(book));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+/*public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   res.send(books[isbn])
- });
+ });*/
+
+
+ function getISBN(isbn) {
+  return new Promise((resolve, reject) => {
+      let num = parseInt(isbn);
+      if (books[num]) {
+          resolve(books[num]);
+      } else {
+          reject({status:404, message:`ISBN ${isbn} not found`});
+      }
+  })
+}
+
+public_users.get('/isbn/:isbn', function (req, res) {
+  getISBN(req.params.isbn)
+  .then(
+      result => res.send(result),
+      error => res.status(error.status).json({message: error.message})
+  );
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
